3a625a7b6a88a6e011ef3d51d721e751dce4e28d
TyronicTrader <82331418+TyronicTrader@users.noreply.github.com>
2023-09-06 01:49:40 -0400

    Changed startup form

--- TradingJournal/TradingJournal.cs
+++ TradingJournal/TradingJournal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TradingJournal
 {
-    public partial class Form1 : Form
+    public partial class TradingJournal : Form
     {
-        public Form1()
+        public TradingJournal()
         {
             InitializeComponent();
         }
