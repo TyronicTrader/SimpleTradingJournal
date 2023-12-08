1f753a2263529c22125201ed6f7c08521111aa37
TyronicTrader <82331418+TyronicTrader@user.noreply.github.com>
2023-12-07 20:32:18 -0500

    did nothing

--- TradingJournal/DBCon.cs
+++ TradingJournal/DBCon.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TradingJournal
