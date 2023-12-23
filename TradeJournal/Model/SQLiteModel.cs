128c1f7a09a285d32885cae23c3f064c49b20ca9
TyronicTrader <82331418+TyronicTrader@user.noreply.github.com>
2023-12-23 13:03:34 -0500

    more WPF updates

---
+++ TradeJournal/Model/SQLiteModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TradeJournal.Model
+{
+    internal class SQLiteModel
+    {
+        public SQLiteConnection Conn;
+        public string dbfileName = "./TheDatabase.sqlite3";
+        public string dbschemaFile = "../../Resources/SQLiteSchema.sql";
+
+        public string connectionstring { get; set; }
+
+        string connection;
+
+        public void getconnection()
+        {
+            connection = $"Data Source={dbfileName};Version=3;Compress=True;FailIfMissing=True;Synchronous=OFF;Journal Mode=WAL;";
+            connectionstring = connection;
+            System.Console.WriteLine(connectionstring);
+        }
+
+
+        public SQLiteModel()
+        {
+            getconnection();
+            Conn = new SQLiteConnection(connectionstring);
+            if (!File.Exists($"{dbfileName}"))
+            {
+                if (!File.Exists($"{dbschemaFile}"))
+                {
+                    MessageBox.Show("Schema File Missing.");
+                    //System.Windows.Forms.Application.Exit();
+                    System.Environment.Exit(1);
+                    //System.Windows.Forms.Application.ExitThread();
+                }
+                else
+                {
+                    SQLiteConnection.CreateFile($"{dbfileName}");
+                    ApplySchema();
+                    SchemaUpdate();
+                }
+            }
+            else
+            {
+                SchemaUpdate();
+                string version = Conn.ServerVersion;
+                System.Console.WriteLine($"DB ALREADY EXISTING SQLite Version: {version} DATABASE FILE AND WILL USE IT");
+            }
+        }
+        public void ConnOpen()
+        {
+            if (Conn.State != System.Data.ConnectionState.Open)
+                Conn.Open();
+        }
+
+        public void ConnClose()
+        {
+            if (Conn.State != System.Data.ConnectionState.Closed)
+                Conn.Close();
+        }
+
+        public void ApplySchema()
+        {
+            if (!File.Exists($"{dbschemaFile}"))
+            {
+                MessageBox.Show("Schema File Missing.");
+            }
+            else
+            {
+                ConnOpen();
+                string dbschema = File.ReadAllText($"{dbschemaFile}");
+                var schemaInit = Conn.CreateCommand();
+                schemaInit.CommandText = dbschema;
+                schemaInit.ExecuteNonQuery();
+                ConnClose();
+            }
+            string version = Conn.ServerVersion;
+            System.Console.WriteLine($"JUST CREATED A SQLite Version: {version} DATABASE FOR YOU");
+        }
+
+        private void SchemaUpdate()
+        {
+            ConnOpen();
+            string dbschemaUpdate = @"CREATE TRIGGER IF NOT EXISTS cleanupEmptyNotes AFTER UPDATE on NOTES BEGIN DELETE from NOTES where Not_NAME = 'Blank Record' AND Not_NOTES = 'Blank Record'; END";
+            var schemaInit = Conn.CreateCommand();
+            schemaInit.CommandText = dbschemaUpdate;
+            schemaInit.ExecuteNonQuery();
+            ConnClose();
+
+        }
+
+    }
+}
